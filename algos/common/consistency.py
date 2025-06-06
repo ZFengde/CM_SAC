247903190adf1645cb22ef47b1019f55ad4006b8
ZFengde <feng.1.zhang@kcl.ac.uk>
2025-06-06 14:02:15 +0100

    1

--- algos/common/consistency.py
+++ algos/common/consistency.py
@@ -154,9 +154,7 @@ class Consistency_Model:
         model_output = model(c_in * x_t, rescaled_t, state)
 
         denoised = c_out * model_output + c_skip * x_t 
-        denoised = th.tanh(denoised) # maybe change into tanh?
-        # since the maximum of c_out is 0.5, output from model is 1
-        # then denoised in [-0.5, 0.5]
+        denoised = th.tanh(denoised) # TODO, think about where to put this part
         return model_output, denoised
     
     def batch_denoise(self, model, x_t, sigmas, state): # get clean output from x_t
@@ -167,9 +165,7 @@ class Consistency_Model:
         model_output = model(c_in * x_t, rescaled_t, state)
 
         denoised = c_out * model_output + c_skip * x_t 
-        denoised = denoised.clamp(-1, 1)
-        # since the maximum of c_out is 0.5, output from model is 1
-        # then denoised in [-0.5, 0.5]
+        denoised = th.tanh(denoised)
         return model_output, denoised
 
     def sample(self, model, state): # get clean output from x_T
